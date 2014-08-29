f7a40f0f2d5977af3f38f0daf7b4cb425a225d37
Roger Mo <roger2016@gmail.com>
2014-08-28 17:35:21 -0700

    add files

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,25 @@
+class EmailsController < ApplicationController
+
+  respond_to :json
+
+  before_filter :allow_shit
+
+  def add_email
+    binding.pry
+    email = params[:email]
+    plan = params[:plan]
+    omment = params[:omment]
+    @email = Email.where(email: email, plan: plan, omment: omment).first
+
+    @email = Email.create(email: email, plan: plan, omment: omment) unless @email
+    respond_with @email
+  end
+
+  private
+  def allow_shit
+    headers['Access-Control-Allow-Origin'] = '0.0.0.0:8000'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+end
